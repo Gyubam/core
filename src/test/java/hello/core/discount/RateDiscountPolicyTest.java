655074255c57a4abdd0d0b8adfc1d7cc725422b3
gyubum <sgb8170@naver.com>
2022-05-05 14:14:44 +0900

    비율 할인 정책 구현

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,42 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10프로 할인")
+    void vip_o() {
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        //when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP 아니면 할인 적용 x")
+    void vip_x() {
+
+        //given
+        Member member = new Member(2L, "memberBasic", Grade.BASIC);
+
+        //when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+}
